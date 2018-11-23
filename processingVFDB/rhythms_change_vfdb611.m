429e66c1cdf06cd9408aaaa9909884fd394c8b51
Nguyen, Minh Tuan <nguyenminhtuan80@gmail.com>
2018-11-23 10:37:08 +0900

    Delete rhythms_change_vfdb611.m

--- processingVFDB/rhythms_change_vfdb611.m
+++
@@ -1,9 +0,0 @@
-function ann_out = rhythms_change_vfdb611(ann,type)
-
-    infor(1).ann = 1; infor(1).type = '(SVTA';      
-    infor(2).ann = ann(2); infor(2).type = type{2};   
-
-for j=1:length(infor)
-  ann_out(j).type = infor(j).type;
-  ann_out(j).sampNum = infor(j).ann;
-end
